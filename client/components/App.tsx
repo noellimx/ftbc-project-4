7cc1799beaa60d4d5e995aaffe759cd72bb58231
Noel Lim <noellimx@gmail.com>
2022-05-01 18:37:20 +0800

    -

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -6,8 +6,25 @@ import {
 
 
 
+interface AppProps {
 
-export const App: React.FC = () => {
+
+
+}
+export const App: React.FC<AppProps> = () => {
+
+  const [isAuth,setAuth] = React.useState<boolean>(false);
+
+
+  React.useEffect(
+    ()=>{
+
+
+
+
+    }
+    ,[]
+  )
  
   return (
     <>
